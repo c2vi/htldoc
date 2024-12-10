8b5d5c952c8c05c664073be96a178f4e2ca97b2a
Sebastian Moser <sebastian@c2vi.dev>
2024-12-10 18:55:07 +0100

    htldoc build theoretically working

--- src/build.rs
+++ src/build.rs
@@ -3,6 +3,8 @@ use std::path::PathBuf;
 
 use clap::ArgMatches;
 
+use crate::utils;
+
 
 pub fn run(sub_matches: &ArgMatches) -> Result<(), String> {
     let mut src_path = std::env::current_dir().map_err(|e| format!("{}", e))?;
@@ -28,6 +30,11 @@ pub fn run(sub_matches: &ArgMatches) -> Result<(), String> {
     template_path_string.pop(); // remove the " at the end
     template_path_string.remove(0); // remove the " at the begining
     let template_path = PathBuf::from(template_path_string);
+
+    utils::copy_dir_all(template_path.as_path().join("diplomarbeit").join("latex_template_htlinn"), build_path.as_path()).expect("error while copying template files into the build folder");
+
+    // print path infos
+    println!("htldocVersion: {}", htldoc_version);
     println!("src_path: {}", src_path.display());
     println!("template_path: {}", template_path.display());
     println!("build_path: {}", build_path.display());
@@ -56,34 +63,38 @@ pub fn run(sub_matches: &ArgMatches) -> Result<(), String> {
         return  Err("failed to eval the $template/diplomarbeit/latex_template_htlinn/template/settings-tex.nix".to_owned());
     }
     let settings_tex = String::from_utf8(settings_tex_output.stdout).expect("not utf8");
-    println!("settings_tex: {}", settings_tex);
-    println!("template_path: {}", template_path.display());
-    println!("htldocVersion: {}", htldoc_version);
-    println!(r#"
-            let 
-                defaultConfig = import {}/diplomarbeit/default-config.nix {{ }};
-                userConfig = import {}/htldoc.nix {{ }};
-                config = defaultConfig // userConfig;
-            in import {}/diplomarbeit/latex_template_htlinn/template/settings-tex.nix config
-        "#, template_path.display(), src_path.display(), template_path.display());
+
     std::fs::write(build_path.as_path().join("template").join("settings.tex"), settings_tex).map_err(|e| format!("from IO err: {}", e))?;
 
 
     // run latex build commands
-    /*
+    // nix shell $nip#pandoc $nip#texlive.combined.scheme-full -c pandoc $infile -o out.pdf --template ~/work/latex/templates/eisvogel/eisvogel.tex --listings
+    // github:nixos/nixpkgs/$(nixos-version --hash)
+    // use the nixpkgs version from the nixos-version command, if available
+    let mut nixpkgs_version: Option<String> = None;
+    let nixpkgs_version_output_result = Command::new("nixos-version")
+        .arg("--hash")
+        .output();
+        ;
+    if Ok(nixpkgs_version_output) = nixpkgs_version_output_result {
+        if !nixpkgs_version_output.status.success() {
+            println!("{}", String::from_utf8(nixpkgs_version_output.stderr).unwrap());
+            return  Err("failed to get the nixpkgs_version with the nixos-version command".to_owned());
+        } else {
+            nixpkgs_version = Some(String::from_utf8(nixpkgs_version_output.stout).expect("not utf8"));
+        }
+    }
+
     let settings_tex = Command::new("nix")
-        .arg("eval")
-        .arg("--impure")
-        .arg("--expr")
-        .arg(format!(r#"
-            let 
-                defaultConfig = import {template_path}/diplomarbeit/default-config.nix {};
-                userConfig = import {src_path}/htldoc.nix {};
-                config = defaultConfig // userConfig;
-            in import {template_path}/diplomarbeit/latex_template_htlinn/template/settings-tex.nix config
-        "#,))
+        .current_dir(build_path.as_path())
+        .arg("shell")
+        .arg(format!("nixpkgs/{}#pandoc", nixpkgs_version))
+        .arg(format!("nixpkgs/{}#texlive.combined.scheme-full", nixpkgs_version))
+        .arg("-c")
+        .arg("pdflatex")
+        .arg("main.tex")
         .output().expect("failed to eval the $template/diplomarbeit/latex_template_htlinn/template/settings-tex.nix")
-    */
+        ;
 
 
 
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,6 @@
 use std::process::Command;
+use std::{io, fs};
+use std::path::Path;
 
 pub fn htldoc_version() -> String {
     let output = Command::new("nix")
@@ -18,4 +20,20 @@ pub fn htldoc_version() -> String {
     htldoc_version.pop(); // remove the " at the end
     htldoc_version.remove(0); // remove the " at the begining
     return htldoc_version;
+}
+
+
+// thanks to: https://stackoverflow.com/a/65192210
+pub fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> {
+    fs::create_dir_all(&dst)?;
+    for entry in fs::read_dir(src)? {
+        let entry = entry?;
+        let ty = entry.file_type()?;
+        if ty.is_dir() {
+            copy_dir_all(entry.path(), dst.as_ref().join(entry.file_name()))?;
+        } else {
+            fs::copy(entry.path(), dst.as_ref().join(entry.file_name()))?;
+        }
+    }
+    Ok(())
 }
